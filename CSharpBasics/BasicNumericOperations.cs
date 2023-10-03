3a4f18dce21fdc1dba50bd52ab506179dc421082
Philip Newman <philipeano@gmail.com>
2023-10-03 15:44:17 +0100

    Solve more basic examples

--- CSharpBasics/BasicNumericOperations.cs
+++ CSharpBasics/BasicNumericOperations.cs
@@ -37,20 +37,78 @@ namespace CSharpBasics
         // Accept an integer from the user, and determine if it is odd or even.
         public bool IsOddNumber(int number)
         {
-
-
+            // Check if it leaves a remainder when divided by 2
+            
+            bool result = (number % 2) != 0;
+            return result;
         } 
 
-
-
-        // Accept the base and height of a triangle, and find the area.
-
+        // Accept the base and height of a triangle, and find the area.        
+        public double CalculateTriangleArea(double baseValue, double heightValue)
+        {
+            return 0.5 * baseValue * heightValue;
+        }
 
         // Accept the length and breadth of a rectangle and find the area.
+        public double CalculateRectangleArea(double length, double breath)
+        {
+            return length * breath;
+        }
 
+        /*
+         * Prompt user for age 
+         * Cast the input into an integer
+         * Check that numeric input is
+            * an integer value, otherwise reject it
+            * within a reasonable range (1 to 150), otherwise reject it
+         * If the age is between 1 and 2, display "Baby"
+         * If the age is between 3 and 12, display "Child"
+         * If the age is between 13 and 19, display "Teenager"
+         * If the age is between 20 and 60, display "Adult"
+         * If the age is 61 and above, display "Senior"
+         * If the age group cannot be determined, display "Unknown"
+        */
 
-        // Given an age value for a user, determine if he/she is a child, teenager, adult, senior.
+        // Given an age value for a user, determine if he/she is a baby, child, teenager, adult, senior.
+        public string DetermineAgeGroup(int age)
+        {
+            string result = "Unknown";
 
+            //// Decision-making using a series of if-else statements
+            //if (age < 1 || age > 150)
+            //{
+            //    Console.WriteLine("The supplied age is not within an acceptable range. Enter a value from 1 to 150.");
+            //    return result;
+            //}
+            //else if (age <= 2)
+            //    return "Baby";
+            //else if (age >= 3 && age <= 12)
+            //    return "Child";
+            //else if (age >= 13 && age <= 19)
+            //    return "Teenager";
+            //else if (age >= 20 && age <= 60)
+            //    return "Adult";
+            //else if (age >= 61 )
+            //    return "Senior";
+
+            // Decision-making using a switch-case statement
+            switch (age)
+            {
+                case >= 1 and <= 2 :
+                    return "Baby";
+                case  >= 3 and  <= 12 :
+                    return "Child";
+                case >= 13 and <= 19:
+                    return "Teenager";
+                case >= 20 and <= 60:
+                    return "Adult";
+                case >= 61 and <= 150:
+                    return "Senior";
+                default:
+                    Console.WriteLine("The supplied age is not within an acceptable range. Enter a value from 1 to 150.");
+                    return result;
+            }
+        }
 
     }
 }
--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -5,14 +5,72 @@ namespace CSharpBasics
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             // Instantiate the BasicStringOperations class and invoke the method
-            // BasicStringOperations stringOperations = new BasicStringOperations();
-            //stringOperations.ConcatenateWords();
-            
+            BasicStringOperations stringOperations = new BasicStringOperations();
+            stringOperations.ConcatenateWords();
+
             // Instantiate the BasicNumericOperations class and invoke the method
             BasicNumericOperations numericOperations = new BasicNumericOperations();
             numericOperations.CheckLeapYear();
+
+            // Prompt the user for an integer
+            Console.WriteLine("Please enter an integer to check if odd or even...");
+            string input = Console.ReadLine();
+
+            // Cast the input into an integer
+            int number = int.Parse(input); // Assuming the user indeed entered a number
+            if (numericOperations.IsOddNumber(number))
+                Console.WriteLine("It is an odd number");
+            else
+                Console.WriteLine("It is an even number");
+
+            // Prompt the user for triangle base and height
+            Console.WriteLine("Please enter a value for base...");
+            string baseStr = Console.ReadLine();
+            Console.WriteLine("Please enter a value for height...");
+            string heightStr = Console.ReadLine();
+
+            // Cast the inputs into double-precision values
+            double baseValue = double.Parse(baseStr); // Assuming the user indeed entered a number
+            double heightValue = double.Parse(heightStr); // Assuming the user indeed entered a number
+            double triangleArea = numericOperations.CalculateTriangleArea(baseValue, heightValue);
+            Console.WriteLine();
+            Console.WriteLine($"The area of the triangle is {triangleArea}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Prompt the user for rectangle length and breadth
+            Console.WriteLine("Please enter a value for length...");
+            string lengthStr = Console.ReadLine();
+
+            Console.WriteLine("Please enter a value for breadth...");
+            string breadthStr = Console.ReadLine();
+
+            // Cast the inputs into double-precision values
+            double lenghthValue = double.Parse(lengthStr); // Assuming the user indeed entered a number
+            double breadthValue = double.Parse(breadthStr); // Assuming the user indeed entered a number
+            double rectangleArea = numericOperations.CalculateRectangleArea(lenghthValue, breadthValue);
+            Console.WriteLine();
+            Console.WriteLine($"The area of the rectangle is {rectangleArea}");             
+           
+            // Prompt the user for their age
+            Console.WriteLine("Please enter your age to determine your age group...");
+            input = Console.ReadLine();
+
+            // Try casting the input into an integer
+            bool isValidInteger = int.TryParse(input, out int age);
+            if (!isValidInteger)
+            {
+                Console.WriteLine("Sorry, the input is not valid for age. Kindly enter an integer.");
+                return;
+            }
+            else
+            {
+                string ageGroup = numericOperations.DetermineAgeGroup(age);
+                Console.WriteLine($"Your age group is {ageGroup}");
+            }
         }
     }
 }
@@ -38,8 +96,10 @@ Problems to solve:
  */
 
 
-
-// private - visible/accessible only within this class
-// public - visible/accessible from anywhere, both within and outside this class
-// protected - visible/accessible both within this class and from any class deriving from this one
-// internal - visible/accessible within this class and any class within the current assembly/project
+// Access modifiers:
+/*
+    private - visible/accessible only within this class
+    public - visible/accessible from anywhere, both within and outside this class
+    protected - visible/accessible both within this class and from any class deriving from this one
+    internal - visible/accessible within this class and any class within the current assembly/project
+ */
